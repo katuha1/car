09f9c37404ade62cceab8a6cbc1aa9ace6036903
Katushkins <103944846+katuha1@users.noreply.github.com>
2023-02-13 14:09:46 +0300

    Delete test_up_git-main directory

--- test_up_git-main/routes/car.route.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const { getCar, postCar, updateCar, deleteCar, getCarOne } = require("../controllers/car.controller");
-const router = Router();
-
-router.route("/").get(getCar).post(postCar);
-router.route("/:id").get(getCarOne).put(updateCar).delete(deleteCar);
-
-module.exports = router;
